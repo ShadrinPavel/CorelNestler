8c1a06ffb134e6f475a6b33733254b56b300c6a3
Pavel M. Shadrin <131566646+ShadrinPavel@users.noreply.github.com>
2026-09-07 08:47:50 +0700

    Upload source: src/test_com.cpp

---
+++ src/test_com.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+
+#import "C:\\Program Files\\Corel\\CorelDRAW Graphics Suite\\26\\Programs64\\TypeLibs\\VGCoreAuto.tlb" no_namespace named_guids \
+    rename("GetCommandLine", "VGGetCommandLine") \
+    rename("CopyFile", "VGCopyFile") \
+    rename("FindWindow", "VGFindWindow") \
+    rename("DrawText", "VGDrawText") \
+    rename("ReplaceText", "VGReplaceText") \
+    rename("GetUserName", "VGGetUserName")
+
+int main() {
+    HRESULT hr = CoInitialize(NULL);
+    if (FAILED(hr)) return 1;
+    try {
+        IVGApplicationPtr pApp(L"CorelDRAW.Application");
+        std::cout << "COM OK" << std::endl;
+    } catch (...) {}
+    CoUninitialize();
+    return 0;
+}
