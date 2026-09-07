c58aba412eec48e9f5112d2e50d75e71a85a0b22
Pavel M. Shadrin <131566646+ShadrinPavel@users.noreply.github.com>
2026-09-07 08:47:43 +0700

    Upload source: src/build_release.bat

---
+++ src/build_release.bat
@@ -0,0 +1,11 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" > NUL
+cd /d "d:\Yandex.Disk\CorelDrawComp\src"
+rc.exe nesting.rc
+if %ERRORLEVEL% neq 0 ( exit /b %ERRORLEVEL% )
+cl.exe /nologo /c /MT /O2 /EHsc /W3 /WX /D UNICODE /D _UNICODE /std:c++17 /I "D:\Yandex.Disk\CorelDrawComp\libs\Clipper2-Clipper2_1.3.0\CPP\Clipper2Lib\include" "D:\Yandex.Disk\CorelDrawComp\libs\Clipper2-Clipper2_1.3.0\CPP\Clipper2Lib\src\clipper.engine.cpp" "D:\Yandex.Disk\CorelDrawComp\libs\Clipper2-Clipper2_1.3.0\CPP\Clipper2Lib\src\clipper.offset.cpp" "D:\Yandex.Disk\CorelDrawComp\libs\Clipper2-Clipper2_1.3.0\CPP\Clipper2Lib\src\clipper.rectclip.cpp"
+if %ERRORLEVEL% neq 0 ( exit /b %ERRORLEVEL% )
+cl.exe /nologo /c /MT /O2 /EHsc /W3 /WX /D UNICODE /D _UNICODE /std:c++17 /I "D:\Yandex.Disk\CorelDrawComp\libs\Clipper2-Clipper2_1.3.0\CPP\Clipper2Lib\include" CorelNester.cpp
+if %ERRORLEVEL% neq 0 ( exit /b %ERRORLEVEL% )
+link.exe /NOLOGO /SUBSYSTEM:WINDOWS /OUT:CorelNester.exe CorelNester.obj clipper.engine.obj clipper.offset.obj clipper.rectclip.obj nesting.res user32.lib ole32.lib oleaut32.lib gdi32.lib gdiplus.lib
+exit /b %ERRORLEVEL%
